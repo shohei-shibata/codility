c461e6aecfa1b534bfbaac17716175d8d369f90a
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-08-14 06:13:31 -0400

    lesson 1 in progress

---
+++ lessons/1/script.js
@@ -0,0 +1,14 @@
+console.log("lesson 1");
+
+const arrToTest = [1, 5, 100]
+
+function BinaryGap(N) {
+  const binary = N.toString(2);
+  return N.toString(2);
+}
+
+arrToTest.forEach(printResult);
+
+function printResult(item) {
+  console.log(BinaryGap(item));
+}
