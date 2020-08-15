d70a54ea3fbb56259aae279d9b2ca61cd5ab41b2
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-08-14 22:02:53 -0400

    lesson 1 complete

--- lessons/1/script.js
+++ lessons/1/script.js
@@ -1,10 +1,21 @@
 console.log("lesson 1");
 
-const arrToTest = [1, 5, 100]
+const arrToTest = [1, 5, 32, 100, 529];
 
 function BinaryGap(N) {
   const binary = N.toString(2);
-  return N.toString(2);
+  let count = 0;
+  let largest = 0;
+  console.log(binary);
+  for (let i=0; i<binary.length; i++) {
+    if (binary[i]==="0") {
+      count++;
+    } else {
+      if (count > largest) { largest = count }
+      count = 0;
+    }
+  }
+  return largest;
 }
 
 arrToTest.forEach(printResult);
